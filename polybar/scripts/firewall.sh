c5bcb16ff6589033ede1b8d041cdec70efcf4828
Costa <142755536+PrinceOfSaiyans@users.noreply.github.com>
2026-01-18 17:57:04 -0500

    Add firewall toggle script
    This script toggles firewall rules using iptables and ip6tables, storing the state in a temporary file.

---
+++ polybar/scripts/firewall.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+STATE_FILE="/tmp/polybar_firewall_toggle_state" # File to store the toggle state
+
+# Initialize state if file doesn't exist
+if [ ! -f "$STATE_FILE" ]; then
+    echo "off" > "$STATE_FILE"
+fi
+# Function to toggle the state
+toggle_state() {
+    CURRENT_STATE=$(cat "$STATE_FILE")
+    if [ "$CURRENT_STATE" == "on" ]; then
+        sudo iptables -F &
+	sudo ip6tables -F &
+	echo "off" > "$STATE_FILE"
+    else
+        sudo iptables-restore < /home/pingu/.config/iptables/ruleset.txt &
+	      sudo ip6tables-restore < home/pingu/.config/iptables/ruleset6.txt &
+        echo "on" > "$STATE_FILE"
+    fi
+}
+# Handle arguments for toggling
+if [ "$1" == "--toggle" ]; then
+    toggle_state
+fi
+# Output content based on current state
+CURRENT_STATE=$(cat "$STATE_FILE")
+if [ "$CURRENT_STATE" == "on" ]; then
+    echo " "
+else
+    echo " "
+fi
